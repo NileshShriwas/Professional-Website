1f1a641b41a115eef9cb13f39f3c1d376206136f
Nilesh Shriwas <nileshshriwas57@gmail.com>
2020-11-25 20:46:28 +0530

    first commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,89 @@
+import React from "react";
+import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
+import { animateScroll as scroll } from 'react-scroll';
+import {
+  FooterContainer,
+  FooterLink,
+  FooterLinkItems,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinkTitle,
+  FooterWrap,
+  SocialIconLink,
+  SocialIcons,
+  SocialLogo,
+  SocialMedia,
+  SocialMediaWrap,
+  WebsiteRights
+} from "./FooterElements";
+
+const Footer = () => {
+  const toggleHome = () => {
+    scroll.scrollToTop();
+}
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <FooterLinksContainer>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>About Us</FooterLinkTitle>
+              <FooterLink to='/signin'>How it works</FooterLink>
+              <FooterLink to='/signin'>Testimonials</FooterLink>
+              <FooterLink to='/signin'>Careers</FooterLink>
+              <FooterLink to='/signin'>Investers</FooterLink>
+              <FooterLink to='/signin'>Terms of Services</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>Contact Us</FooterLinkTitle>
+              <FooterLink to='/signin'>Contact</FooterLink>
+              <FooterLink to='/signin'>Support</FooterLink>
+              <FooterLink to='/signin'>Destinations</FooterLink>
+              <FooterLink to='/signin'>Sponsorships</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>Videos</FooterLinkTitle>
+              <FooterLink to='/signin'>Submit video</FooterLink>
+              <FooterLink to='/signin'>Ambassadors</FooterLink>
+              <FooterLink to='/signin'>Agency</FooterLink>
+              <FooterLink to='/signin'>Influencer</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>Social Media</FooterLinkTitle>
+              <FooterLink to='/signin'>Instagram</FooterLink>
+              <FooterLink to='/signin'>Facebook</FooterLink>
+              <FooterLink to='/signin'>Youtube</FooterLink>
+              <FooterLink to='/signin'>Twitter</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+        </FooterLinksContainer>
+        <SocialMedia>
+            <SocialMediaWrap>
+                <SocialLogo to='/' onClick={toggleHome}>
+                    ANUSHKA WEB
+                </SocialLogo>
+                <WebsiteRights>Anushka Web © {new Date().getFullYear()} ALL rights reserved.</WebsiteRights>
+                <SocialIcons>
+                    <SocialIconLink href='/' target='_blank' aria-label='Facebook'>
+                        <FaFacebook />
+                    </SocialIconLink>
+                    <SocialIconLink href='/' target='_blank' aria-label='Instagram'>
+                        <FaInstagram />
+                    </SocialIconLink>
+                    <SocialIconLink href='/' target='_blank' aria-label='Twitter'>
+                        <FaTwitter />
+                    </SocialIconLink>
+                    <SocialIconLink href='/' target='_blank' aria-label='Youtube'>
+                        <FaYoutube />
+                    </SocialIconLink>
+                </SocialIcons>
+            </SocialMediaWrap>
+        </SocialMedia>
+      </FooterWrap>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
